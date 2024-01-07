5d19895f110fb78e1370b7a08eccb7c9d1405bdd
grega1 <adrianogrego1@gmail.com>
2024-01-07 22:30:56 +0000

    feat: onchange functionality

--- src/components/Form/AddPostForm.jsx
+++ src/components/Form/AddPostForm.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react"
 
-export default function AddPostForm(){
+export default function AddPostForm(){4
+    const [title,setTitle] = useState('')
+    const [content,setContent] = useState('')
+
+    const handleTitleChanged = function(event){
+        setTitle(event.target.value)
+    }
+    const handleContentChanged = function(event){
+        setContent(event.target.value)
+    }
     return(
         <section>
             <h2>Add a New Post</h2>
         <form>
             <label htmlFor="postTitle">Post Title:</label>
-            <input type="text" name="postTitle" id="postTitle" />
+            <input type="text" value={title} onChange={handleTitleChanged} name="postTitle" id="postTitle" />
             <label htmlFor="postContent">Post Content:</label>
-            <input type="text" name="postContent" id="postContent" />
+            <input type="text" name="postContent" value={content} onChange={handleContentChanged}id="postContent" />
 
         </form>
         </section>
