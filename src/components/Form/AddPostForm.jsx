14b04884a2ea001c823d3f6d2a1653c9aa9e1a3c
grega1 <adrianogrego1@gmail.com>
2024-01-08 22:43:40 +0000

    feat: savepost functionality

--- src/components/Form/AddPostForm.jsx
+++ src/components/Form/AddPostForm.jsx
@@ -1,25 +1,52 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-export default function AddPostForm(){4
-    const [title,setTitle] = useState('')
-    const [content,setContent] = useState('')
+import { postAdded } from "../../features/posts/postsSlice";
 
-    const handleTitleChanged = function(event){
-        setTitle(event.target.value)
+export default function AddPostForm() {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleTitleChanged = function (event) {
+    setTitle(event.target.value);
+  };
+  const handleContentChanged = function (event) {
+    setContent(event.target.value);
+  };
+
+  const handleSavePost = () => {
+    if (title && content) {
+      dispatch(postAdded(title, content));
+      setContent("");
+      setTitle("");
     }
-    const handleContentChanged = function(event){
-        setContent(event.target.value)
-    }
-    return(
-        <section>
-            <h2>Add a New Post</h2>
-        <form>
-            <label htmlFor="postTitle">Post Title:</label>
-            <input type="text" value={title} onChange={handleTitleChanged} name="postTitle" id="postTitle" />
-            <label htmlFor="postContent">Post Content:</label>
-            <input type="text" name="postContent" value={content} onChange={handleContentChanged}id="postContent" />
+  };
 
-        </form>
-        </section>
-    )
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={handleTitleChanged}
+          name="postTitle"
+          id="postTitle"
+        />
+        <label htmlFor="postContent">Post Content:</label>
+        <input
+          type="text"
+          name="postContent"
+          value={content}
+          onChange={handleContentChanged}
+          id="postContent"
+        />
+        <button type="button" onClick={handleSavePost}>
+          Save Post
+        </button>
+      </form>
+    </section>
+  );
 }
