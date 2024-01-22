860435e44ebbb7bf1f6bbf28a018a5456bbdff37
grega1 <adrianogrego1@gmail.com>
2024-01-22 22:21:21 +0000

    feat: adding post time on posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,15 +3,23 @@ import './Post.css'
 import { useSelector } from 'react-redux'
 
 import { selectAllUsers } from '../../features/users/usersSlice'
+import { parseISO, formatDistanceToNow } from 'date-fns'
 
 export default function Post({props}){
     const users = useSelector(selectAllUsers)
     const author = users.find(user => user.id === props.userId)
+
+    let timeAgo = ''
+    if(props.date){
+        const date= parseISO(props.date)
+        const timePeriod = formatDistanceToNow(date)
+        timeAgo = `${timePeriod} ago` 
+    }
     return(
         <article className='post'>
             <h2>{props.title}</h2>
             <p>{props.content}</p>
-            <span> by{author ? author.name : " Unknow author"}</span>
+            <p><span> by{author ? author.name : " Unknow author"}</span><span title={props.date}>{timeAgo}</span> </p>
         </article>
     )
 }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,16 @@
-import {createSlice, nanoid} from "@reduxjs/toolkit"
+import {createSlice, nanoid} from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 
 const initialState = [
     {
-        id:'1',title:"Exploring Redux", content:"Lorem ipsum dolor sit amet, consectetur adip"
+        id:'1',title:"Exploring Redux", content:"Lorem ipsum dolor sit amet, consectetur adip",date:sub(new Date(),{minutes:10}).toISOString()
     },
     {
-        id:'2',title:"Redux Toolkits", content:"Lorem ipsum dolor sit amet, consectetur adip"
+        id:'2',title:"Redux Toolkits", content:"Lorem ipsum dolor sit amet, consectetur adip",date:sub(new Date(),{minutes:5}).toISOString()
     },
     {
-        id:'3',title:"Slices", content:"Lorem ipsum dolor sit amet, consectetur adip"
+        id:'3',title:"Slices", content:"Lorem ipsum dolor sit amet, consectetur adip", date:sub(new Date(),{minutes:2}).toISOString()
     }
 
 ]
@@ -27,7 +28,8 @@ const postsSlice= createSlice({
                     id:nanoid(),
                     title,
                     content,
-                    userId
+                    userId,
+                    date:new Date().toISOString()
                 }
             }
         }}
