9c9b2d100a79da46dd324db32c42251a5121176e
grega1 <adrianogrego1@gmail.com>
2024-01-23 21:54:18 +0000

    feat: adding like functionality

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,40 +1,60 @@
-import {createSlice, nanoid} from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 
-
 const initialState = [
-    {
-        id:'1',title:"Exploring Redux", content:"Lorem ipsum dolor sit amet, consectetur adip",date:sub(new Date(),{minutes:10}).toISOString()
-    },
-    {
-        id:'2',title:"Redux Toolkits", content:"Lorem ipsum dolor sit amet, consectetur adip",date:sub(new Date(),{minutes:5}).toISOString()
-    },
-    {
-        id:'3',title:"Slices", content:"Lorem ipsum dolor sit amet, consectetur adip", date:sub(new Date(),{minutes:2}).toISOString()
-    }
+  {
+    id: "1",
+    title: "Exploring Redux",
+    content: "Lorem ipsum dolor sit amet, consectetur adip",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    like:0
+  },
+  {
+    id: "2",
+    title: "Redux Toolkits",
+    content: "Lorem ipsum dolor sit amet, consectetur adip",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    like:0
+  },
+  {
+    id: "3",
+    title: "Slices",
+    content: "Lorem ipsum dolor sit amet, consectetur adip",
+    date: sub(new Date(), { minutes: 2 }).toISOString(),
+    like:0
+  },
+];
 
-]
-
-const postsSlice= createSlice({
-    name:'posts',
-    initialState,
-    reducers:{
-        postAdded:{
-            reducer(state,action){
-            state.push(action.payload)
-        }, prepare(title,content, userId){
-            return{
-                payload:{
-                    id:nanoid(),
-                    title,
-                    content,
-                    userId,
-                    date:new Date().toISOString()
-                }
-            }
-        }}
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            userId,
+            date: new Date().toISOString(),
+            like:0
+          },
+        };
+      },
+    },
+    addLike(state,action){
+        const {postId} = action.payload;
+        const existingPost = state.find(post => post.id ===postId);
+        if(existingPost){
+            existingPost.like++;
+        }
     }
-})
-export const selectAllPosts = (state) => state.posts
-export const {postAdded} = postsSlice.actions
-export default postsSlice.reducer
+  },
+});
+export const selectAllPosts = (state) => state.posts;
+export const { postAdded, addLike} = postsSlice.actions;
+export default postsSlice.reducer;
